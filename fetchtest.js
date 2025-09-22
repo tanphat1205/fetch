eb0d303963093e42d3cf27b8a24751b1b07267f1
tanphat1205 <phatle12052003@gmail.com>
2025-09-22 11:00:27 +0700

    Update fetchtest.js
    tao sửa 123

--- fetchtest.js
+++ fetchtest.js
@@ -1,6 +1,6 @@
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then(response => response.json())
   .then(data => {
-    console.log("Danh sách bài viết:", data);
+    console.log("Danh sách bài viết123:", data);
   })
   .catch(error => console.error("Lỗi:", error));
